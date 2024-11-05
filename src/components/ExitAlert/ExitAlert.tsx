8c1617b04789f9ad2e2af7e410911e6defe24fee
HP <vai.katia03@mail.ru>
2024-11-05 18:46:28 +0300

    refactor: modals

--- src/components/ExitAlert/ExitAlert.tsx
+++ src/components/ExitAlert/ExitAlert.tsx
@@ -3,15 +3,18 @@ import { Button } from '@components/Button/Button';
 import { logoutRequest } from '@store/actions/authActions';
 
 import './styles.scss';
-export const ExitAlert = () => {
+export const ExitAlert = ({ onCloseModal }: { onCloseModal?: () => void }) => {
   const dispatch = useDispatch();
   const handleLogOut = () => {
     dispatch(logoutRequest());
   };
   return (
-    <div>
+    <div className="exit-alert">
       <p>Are you sure you want to exit?</p>
-      <Button onClick={handleLogOut} text="Log Out" />
+      <div className="exit-alert-btns">
+        <Button onClick={handleLogOut} text="Log Out" />
+        <Button onClick={onCloseModal} text="Cancel" />
+      </div>
     </div>
   );
 };
